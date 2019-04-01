ba4538572137b2e0211ccbc00d66f666a38b0486
upolisIG <49081051+upolisIG@users.noreply.github.com>
2019-04-01 18:04:02 -0500

    Includes body parser

--- handler.js
+++ handler.js
@@ -1,12 +1,17 @@
 "use strict";
 
 const serverless = require('serverless-http');
+const bodyParser = require("body-parser");
 const express = require('express');
 const HttpStatus = require("http-status-codes");
 const app = express()
 const validate = require("./validate").validate;
 
-app.get('/', function (req, res) {
+const PATH = "/"; // Replace by service path referenced in serverless.yml
+
+app.use(bodyParser.json({ strict: false }));
+
+app.get(PATH, function (req, res) {
   let respose = validate(req.body);
 
   if (respose.code === HttpStatus.OK) {
